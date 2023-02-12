6c9910114ae594ab5ab9c5962f9dc39bd536c777
Jeronimo <jeronimocp@ucit.ar>
2023-02-12 16:48:56 -0300

    reformulando navbar

---
+++ src/components/NavBar/LogoPrincipal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './logoprincipal.css';
+import  logonavbar from './Logo-sin-nombre-blanco.png';
+
+const LogoPrincipal = ()=>{
+    return <img src={logonavbar} alt=""
+    className="logoprincipal" />;
+
+};
+export default LogoPrincipal;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+import estilo from 'styled-components';
+
+
+function NavBar() {
+  return (
+  <> 
+  <NavBarConts>
+     Este es el navBar
+  </NavBarConts>
+ 
+  </>
+  )
+};
+
+export default NavBar;
+
+const NavBarConts = styled.nav
